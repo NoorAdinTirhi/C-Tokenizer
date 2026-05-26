075b001478eaa9730b24dbfd1d0b862c524fe926
NoorAdinTirhi <nooradintirhi@gmail.com>
2026-05-26 14:02:31 +0300

    reorganized state transitions a bit

--- src/headers/fsm.h
+++ src/headers/fsm.h
@@ -3,6 +3,7 @@
 #include "tokens.h"
 #include "source_ref.h"
 using namespace std;
+struct parse_info;
 
 struct fsm {
     parser_state current_state;
@@ -13,4 +14,21 @@ struct fsm {
     void transition(parser_state new_state);
     parser_state get_current_state() const;
     vector<token*> parse();
+
+    void start_state(parse_info* info);
+    void whitespace_state(parse_info* info);
+    void identifier_state(parse_info* info);
+    void preprocessor_directive_state(parse_info* info);
+};
+
+struct parse_info {
+    vector<token*> token_stream;
+    char current_char;
+    string current_string;
+    token* current_token;
+
+    bool string_has_underscore;
+
+    parse_info();
+    void reset();
 };
--- src/impl/fsm.cc
+++ src/impl/fsm.cc
@@ -13,54 +13,99 @@ void fsm::transition(parser_state new_state) {
 }
 
 vector<token*> fsm::parse() {
-    vector<token*> token_stream;
     block_iterator it = this->block->get_iterator();
-    char curr_char;
-    string curr_string;
-    token *curr_token;
+    
+    parse_info info;
 
     while (it.has_next()){
-        curr_char = it.next();
+        info.current_char = it.next();
 
         switch (current_state) {
             case parser_state::START:
-                if (curr_char == ' ' || curr_char == '\t' || curr_char == '\n' || curr_char == '\r') {
-                    transition(parser_state::IN_WHITESPACE);
-                    break;
-                }
+                start_state(&info);
+                break;
             case parser_state::IN_WHITESPACE:
-                if (isalpha(curr_char) || curr_char == '_') {
-                    curr_string += curr_char;
-                    transition(parser_state::IN_IDENTIFIER);
-                }else if (curr_char == '#'){
-                    transition(parser_state::IN_PREPROCESSOR_DIRECTIVE);
-                }
+                whitespace_state(&info);
                 break;
+            
             case parser_state::IN_IDENTIFIER:
-                if (isalnum(curr_char) || curr_char == '_') {
-                    curr_string += curr_char;
-                } else {
-                    curr_token = new identifier_token(curr_string, it.current_ref());
-                    token_stream.push_back(curr_token);
-                    curr_string.clear();
-                    if (it.has_prev()) it.prev();
-                    transition(parser_state::START);
-                }
+                identifier_state(&info);
                 break;
+            
             case parser_state::IN_PREPROCESSOR_DIRECTIVE:
-                if (isalpha(curr_char)) {
-                    curr_string += curr_char;
-                }else if (curr_char == '"' || curr_char == '<' || curr_char == '\'' || curr_char == ' ' || curr_char == '\t'){
-                    if (preprocessor_directive_token::directive_map.find(curr_string) != preprocessor_directive_token::directive_map.end()) {
-                        curr_token = new preprocessor_directive_token(curr_string, it.current_ref());
-                        token_stream.push_back(curr_token);
-                    } else {
-                        throw invalid_argument("Expected preprocessor directive : " + curr_string + " at " + it.current_ref().toString());
-                    }
-                }
+                preprocessor_directive_state(&info);
+                break;
             default:
                 break;
         }
     }
-    return token_stream;
+    return info.token_stream;
+}
+
+void fsm::start_state(parse_info* info) {
+    if (info->current_char == ' ' || info->current_char == '\t' || info->current_char == '\n' || info->current_char == '\r') {
+        transition(parser_state::IN_WHITESPACE);
+    } else if (isalpha(info->current_char) || info->current_char == '_') {
+        info->current_string += info->current_char;
+        transition(parser_state::IN_IDENTIFIER);
+    } else if (info->current_char == '#') {
+        transition(parser_state::IN_PREPROCESSOR_DIRECTIVE);
+    }
+    //support the other states
+};
+
+void fsm::whitespace_state(parse_info* info) {
+    if (info->current_char == ' ' || info->current_char == '\t' || info->current_char == '\n' || info->current_char == '\r') {
+        // stay in whitespace
+    } else if (isalpha(info->current_char) || info->current_char == '_') {
+        info->current_string += info->current_char;
+        transition(parser_state::IN_IDENTIFIER);
+    } else if (info->current_char == '#') {
+        transition(parser_state::IN_PREPROCESSOR_DIRECTIVE);
+    }
+    //support the other states
+};
+
+void fsm::identifier_state(parse_info* info) {
+    block_iterator it = this->block->get_iterator();
+    
+    if (isalnum(info->current_char) || info->current_char == '_') {
+        info->current_string += info->current_char;
+    } else {
+        info->current_token = new identifier_token(info->current_string, it.current_ref());
+        info->token_stream.push_back(info->current_token);
+        info->reset();
+        if (it.has_prev()) it.prev();
+        transition(parser_state::START);
+    }
+};
+
+void fsm::preprocessor_directive_state(parse_info* info) {
+    block_iterator it = this->block->get_iterator();
+    if (isalpha(info->current_char)) {
+        info->current_string += info->current_char;
+    }else if (info->current_char == '"' || info->current_char == '<' || info->current_char == '\'' || info->current_char == ' ' || info->current_char == '\t'){
+        if (preprocessor_directive_token::directive_map.find(info->current_string) != preprocessor_directive_token::directive_map.end()) {
+            info->current_token = new preprocessor_directive_token(info->current_string, it.current_ref());
+            info->token_stream.push_back(info->current_token);
+        } else {
+            throw invalid_argument("Expected preprocessor directive : " + info->current_string + " at " + it.current_ref().toString());
+        }
+    }
+};
+
+
+parse_info::parse_info() {
+    this->token_stream = vector<token*>();
+    this->current_char = '\0';
+
+    this->current_string = "";
+    this->current_token = nullptr;
+    this->string_has_underscore = false;
+}
+
+void parse_info::reset() {
+    this->current_string = "";
+    this->current_token = nullptr;
+    this->string_has_underscore = false;
 }
